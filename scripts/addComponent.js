dea5024f5032e31c0d87cac3a7cb66c433ac859f
Brian Bartholomew <me@bcbrian.com>
2018-03-23 09:43:44 -0500

    getting the initial flow setup

--- scripts/addComponent.js
+++ scripts/addComponent.js
@@ -1,29 +1,50 @@
+const fs = require('fs-extra');
 const program = require('commander');
 const { prompt } = require('inquirer'); // require inquirerjs library
 
+async function setupNewComponent(componentType, componentName) {
+  try {
+    await fs.copy(
+      './scripts/addComponentStarter/Component',
+      `./src/${componentType}s/${componentName}`,
+      {
+        overwrite: false,
+        errorOnExist: true
+      }
+    );
+    console.log('success!');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 const questions = [
-  {
-    type: 'input',
-    name: 'componentType',
-    message: 'What type of component are you making?'
-  },
   {
     type: 'input',
     name: 'componentName',
     message: 'What do you want to call this component?'
   },
+  {
+    type: 'list',
+    name: 'componentType',
+    message: 'What type of component are you making?',
+    choices: ['atom', 'molecule', 'organism', 'template', 'page']
+  }
 ];
 
 program.version('0.0.1').description('floorplan Util');
 
 program
-  .command('addNewComponent') // No need of specifying arguments here
+  .command('addComponent')
   .alias('a')
   .description('Add a componet')
   .action(() => {
-    prompt(questions).then(answers =>
-      console.log(JSON.stringify(answers, null, 2))
-    );
+    prompt(questions).then(async answers => {
+      console.log(
+        `Creating ${answers.componentType} component ${answers.componentName}`
+      );
+      await setupNewComponent(answers.componentType, answers.componentName);
+    });
   });
 
 program.parse(process.argv);
